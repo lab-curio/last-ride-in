c1a110f5bb7ffc41f6789d339b745503f664a6cc
mathisonian <mc@mathisonian.com>
2012-06-24 15:15:31 -0400

    Added server, routes

---
+++ app.js
@@ -0,0 +1,36 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/:agency_id?', routes.index);
+app.get('/:agency_id/:route_id', routes.route);
+app.get('/:agency_id/:route_id/:stop_id', routes.stop);
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+
+/*
+ * GET the index page.
+ */
+
+exports.index = function(req, res){
+	res.render('index', {
+		title : "last ride in"
+	});
+};
+
+exports.route = function(req, res){
+
+};
+
+exports.stop = function(req, res){
+
+};
