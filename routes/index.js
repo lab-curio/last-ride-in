60d7a22e6ad01af78a8c52594ed2f72860b1032e
Ethan <ethan@ubuntu.(none)>
2012-06-24 20:17:52 -0400

    * fix missing slash and extra s in jade route, added data fetching for exports.stop

--- routes/index.js
+++ routes/index.js
@@ -10,20 +10,7 @@ exports.index = function(req, res){
 	});
 };
 
-exports.route = function(req, res){
-	/*
-	 * Uncomment that stuff once the data fetching is implemented
-	 */
-	
-	// dataGetter.getData(req.params, function(error, data) {
-	// 	res.render('route', {
-	// 		title : req.params.route_id + ' - last ride in',
-	// 		locals : {
-	// 			stops : data
-	// 		}
-	// 	})
-	// });
-	
+exports.route = function(req, res){	
 	var agency_id = req.params.agency_id
       , route_id = req.params.route_id;
       
@@ -43,13 +30,34 @@ exports.route = function(req, res){
 	});
 };
 
-// exports.stop = function(req, res){
-// 	dataGetter.getData(req.params, function(error, data) {
-// 		res.render('stop', {
-// 			title : req.params.route_id + ' - ' + req.params.stop_id,
-// 			locals : {
-// 				times : data
-// 			}
-// 		})
-// 	});
-// };
+ exports.stop = function(req, res){
+	var agency_id = req.params.agency_id
+      , route_id = req.params.route_id
+      , stop_id = req.params.stop_id
+      , north_stop_id
+      , south_stop_id;	
+      
+	// stops are split into northbound and southbound stops.
+	// ex: G22S and G22N	  
+	
+    if (stop_id.substr(-1) === "N") {
+		north_stop_id = stop_id;
+		south_stop_id = stop_id.substring(0,stop_id.length-1) + "S";
+	} else {
+		north_stop_id = stop_id.substring(0,stop_id.length-1) + "N";
+		south_stop_id = stop_id;
+	}
+	 
+	gtfs.getTimesByStop(agency_id, route_id, north_stop_id, 0, function(e, northData) {
+		gtfs.getTimesByStop(agency_id, route_id, south_stop_id, 1, function(e, southData) {
+		console.log(northData + southData);
+		
+ 		res.render('stop', {
+ 			title : req.params.route_id + ' - ' + req.params.stop_id,
+ 			locals : {
+ 				times : data
+ 			}
+ 		})
+		});
+ 	});
+ };
